28580a2a172c890feec1423657958d8e3fa775ad
Pouya-eb <Pouyaebrahimi1379@gmail.com>
2023-06-27 08:37:32 +0330

    Adding SparseMatrix constructor (2nd version)

--- sparsematrix.h
+++ sparsematrix.h
@@ -7,6 +7,7 @@ template <typename T>
 class SparseMatrix {
 public:
     SparseMatrix(int, int);
+    SparseMatrix(const SparseMatrix<T>&);
 
 private:
     class Node {
--- sparsematrix.hpp
+++ sparsematrix.hpp
@@ -6,3 +6,16 @@ SparseMatrix<T>::SparseMatrix(int rows, int columns)
     , matrix_column { columns }
 {
 }
+template <typename T>
+SparseMatrix<T>::SparseMatrix(const SparseMatrix<T>& other)
+{
+    head = new Node { *other.head };
+    Node* _head { head };
+    Node* __head { other.head };
+    while ((__head = __head->next)) {
+        _head->next = new Node { *__head };
+        _head = _head->next;
+    }
+    matrix_row = other.matrix_row;
+    matrix_column = other.matrix_column;
+}
